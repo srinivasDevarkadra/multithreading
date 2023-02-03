b5bac4872b5d490fea3e8510c144f73c9c151dcd
Srinivas D <srinivas.d@dpworld.com>
2023-02-03 20:46:59 +0530

    Added my project

---
+++ src/RateLimiting.java
@@ -0,0 +1,12 @@
+public class RateLimiting {
+    public static void main(String[] args) {
+        System.out.println("hello world from srinivas ");
+
+        //bucket filled with tokens at the rate of one token per second
+        //bucket can hold maximum of N tokens
+
+        //consider it as modified version of producer consumer problem
+
+
+    }
+}
