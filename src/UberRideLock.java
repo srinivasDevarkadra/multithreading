54bf36170dfeed350f44c5fcfba83cad1cfc15ec
Srinivas D <srinivas.d@dpworld.com>
2023-02-08 18:00:06 +0530

    uber_ride_lock

---
+++ src/UberRideLock.java
@@ -0,0 +1,55 @@
+public class UberRideLock {
+    //can ride all democrats or republicans
+    //two democrats and two republicans
+    //seated method to request for a seat in a car
+    //drive once all drivers seated any on the thread can inform to start the drive
+
+    //total seats occupied
+    //republican count occupied
+    //democrat count occupied
+    int total_seats_occupied = 0;
+    int rep_seats_occupied = 0;
+    int demo_seats_occupied = 0;
+
+    public synchronized void republicanSeated() throws InterruptedException {
+        while(total_seats_occupied > 4){
+            wait();
+        }
+        while(total_seats_occupied > 2 && demo_seats_occupied >2) {
+            wait();
+        }
+        while(total_seats_occupied == 3 && demo_seats_occupied == 1) {
+            wait();
+        }
+        rep_seats_occupied++;
+        total_seats_occupied++;
+        if(total_seats_occupied == 4){
+            total_seats_occupied = 0;
+            rep_seats_occupied=0;
+            demo_seats_occupied=0;
+            notifyAll();
+        }
+    }
+
+    public synchronized void democratSeated() throws InterruptedException {
+        while (total_seats_occupied>4) {
+            wait();
+        }
+        while(total_seats_occupied > 2 && rep_seats_occupied >2) {
+            wait();
+        }
+        while(total_seats_occupied == 3 && rep_seats_occupied == 1) {
+            wait();
+        }
+        demo_seats_occupied++;
+        total_seats_occupied++;
+        if(total_seats_occupied == 4) {
+            total_seats_occupied = 0;
+            rep_seats_occupied=0;
+            demo_seats_occupied=0;
+            notifyAll();
+        }
+    }
+
+
+}
